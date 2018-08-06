e968c8dec5782abaae19518e338993c300beb9a7
Ben Kiggen and Hassan Al-khalifah <ben@>
2018-08-06 14:28:05 -0700

    work out js kinks, add css, final product

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 //weight / height squared
+var answer;
 
 var bmi = function(weight, height){
   return weight / (height * height);
@@ -28,9 +29,27 @@ var getHeightData = function(){
   }
 }
 
+var subanswer = function(){
+  if (answer <= 18.5){
+    return "underweight";
+  } else if (answer > 18.5 && answer < 25){
+    return "normal";
+  } else if (answer >= 25 && answer < 30){
+    return "overweight";
+  } else if (answer >= 30 && answer < 35){
+    return "obese";
+  } else if (answer >= 35 && answer < 40){
+    return "obese";
+  } else {
+    return "extremely obese";
+  }
+}
+
 var run = function(){
   var weight = getWeightData();
   var height = getHeightData();
-  var answer = bmi(weight, height);
+  answer = bmi(weight, height);
   document.getElementById("result").innerHTML = "Your BMI is " + answer.toFixed(0) + ".";
+  var answer2 = subanswer();
+  document.getElementById("explanation").innerHTML = "The BMI folks would say you are " + answer2 + ".";
 }
