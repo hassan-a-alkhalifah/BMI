ef18aef800eab24389681ec98033182f0ed66843
Ben Kiggen and Hassan Al-khalifah <hassan@>
2018-08-06 13:28:39 -0700

    js work, site is functioning as planned

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,8 @@ var getWeightData = function(){
   var measureweight = document.getElementById("kilo").checked;
 
   if (measureweight === true){
-    //nothing
-    return parseFloat(document.getElementById("weight").value);
+    return document.getElementById("weight").value;
   } else {
-    //convert to kg
     var wt = parseFloat(document.getElementById("weight").value);
     return wt * 0.45359237;
   }
@@ -21,19 +19,18 @@ var getHeightData = function(){
   var measureheight = document.getElementById("cent").checked;
 
   if (measureheight === true){
-    //nothing
-    return parseFloat(document.getElementById("height").value);
+    var htcent = parseFloat(document.getElementById("height").value);
+    return htcent / 100;
   } else {
     //convert to kg
     var ht = parseFloat(document.getElementById("height").value);
-    return ht * 2.54;
+    return (ht / 100) * 2.54;
   }
 }
 
-var weight = getWeightData();
-var height = getHeightData();
-
-console.log(height);
-
-
-getElementById("result").innerHTML = "Your BMI is";
+var run = function(){
+  var weight = getWeightData();
+  var height = getHeightData();
+  var answer = bmi(weight, height);
+  document.getElementById("result").innerHTML = "Your BMI is " + answer.toFixed(0) + ".";
+}
